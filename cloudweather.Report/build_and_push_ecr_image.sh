e20f9f0f188d4b699fd5e81fad02390341c87d7c
HOAINHAN\Admin <phamhoainhan1989@gmail.com>
2022-10-05 13:37:48 +0700

    edit cofig

--- cloudweather.Report/build_and_push_ecr_image.sh
+++ cloudweather.Report/build_and_push_ecr_image.sh
@@ -1,6 +1,6 @@
 ﻿set -e
 
-aws ecr get-login-password --region ap-southeast-1 --profile weather-ecr-agent | docker login --username AWS --password-stdin 633282583713.dkr.ecr.ap-southeast-1.amazonaws.com
+aws ecr get-login-password --region ap-southeast-1 --profile weather_ecr_agent | docker login --username AWS --password-stdin 633282583713.dkr.ecr.ap-southeast-1.amazonaws.com
 docker build -f ./Dockerfile -t cloud-weather-report:latest .
 docker tag cloud-weather-report:latest 633282583713.dkr.ecr.ap-southeast-1.amazonaws.com/cloud-weather-report:latest
 docker push 633282583713.dkr.ecr.ap-southeast-1.amazonaws.com/cloud-weather-report:latest
